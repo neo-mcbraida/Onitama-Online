2b1a88672bc8739d746bd9b01297175431ce4d02
neo Mcbraida <nsm.neo@gmail.com>
2020-08-03 12:09:17 +0100

    multiplayer dragging works

--- newchatpage/wwwroot/js/Grid.js
+++ newchatpage/wwwroot/js/Grid.js
@@ -20,11 +20,11 @@
         this.p9 = new Pawn(document.querySelector("#item9"), connection, 9, 1, 0);
         this.p10 = new Pawn(document.querySelector("#item10"), connection, 10, 1, 0);
 
-        this.c1 = new Card([-1, 1, 0], [-1, 1, 2], 0, 'url("/assets/bat.jpg")', document.querySelector("#card1"), connection, 1);
-        this.c2 = new Card([1, -1, -1, 1], [-2, -1, 1, 2], 1, 'url("/assets/bison.jpg")', document.querySelector("#card2"), connection, 2);
-        this.c3 = new Card([-1, 1, 0], [0, 0, 1], 0, 'url("/assets/bear.jpg")', document.querySelector("#card3"), connection, 3);
-        this.c4 = new Card([0, 1, 0], [-2, 0, 2], 1, 'url("/assets/crawler.jpg")', document.querySelector("#card4"), connection, 4);
-        this.c5 = new Card([0, 1, 0], [-1, 0, 1], 1, 'url("/assets/cow.jpg")', document.querySelector("#card5"), connection, 5);
+        this.c1 = new Card([-1, 1, 0], [-1, 1, 2], 0, 'url("/assets/bat.jpg")', document.querySelector("#card1"), connection, 11);
+        this.c2 = new Card([1, -1, -1, 1], [-2, -1, 1, 2], 1, 'url("/assets/bison.jpg")', document.querySelector("#card2"), connection, 12);
+        this.c3 = new Card([-1, 1, 0], [0, 0, 1], 0, 'url("/assets/bear.jpg")', document.querySelector("#card3"), connection, 13);
+        this.c4 = new Card([0, 1, 0], [-2, 0, 2], 1, 'url("/assets/crawler.jpg")', document.querySelector("#card4"), connection, 14);
+        this.c5 = new Card([0, 1, 0], [-1, 0, 1], 1, 'url("/assets/cow.jpg")', document.querySelector("#card5"), connection, 15);
 
         //this.c1 = new Card([-1, 1, 0], [-1, 1, 2], document.querySelector("#card1"), 0, 'url("/assets/bat.jpg")');//0 = red, 1 = blue
         //this.c2 = new Card([1, -1, -1, 1], [-2, -1, 1, 2], document.querySelector("#card2"), 1, 'url("/assets/bison.jpg")');
@@ -39,12 +39,17 @@
         this.playerCard = [this.c1, this.c2];
         this.opponentCard = [this.c4, this.c5];
 
+
+
         this.player = [this.p1, this.p2, this.p3, this.p4, this.p5]
         this.opponent = [this.p6, this.p7, this.p8, this.p9, this.p10]
         this.canMove = true;
 
+        this.pieces = [];//using only array of opponent pieces, as do not need to set active for clients side pieces
+
         this.activeItem = new Piece(null, connection, 0);
-        this.pIndex = 0;
+        this.pIndex1 = 0;
+        this.pIndex2 = 0;
         this.domElement;
     }
 
@@ -83,6 +88,10 @@
         var roomId = this.roomId;
         var container = this.container;
         var deck = this.playerDeck;
+        
+
+        this.pieces.push(this.opponentCard);//push cards 1st as it is the shorter array
+        this.pieces.push(this.opponent);
 
         for (var i = 0; i < 2; i++) {
             var playcard = this.playerCard[i];
@@ -170,29 +179,39 @@
 
     }
 
-    SetActive(_pawn) {
-        var pawns = this.opponent;
-        var i;
-        for (i = 0; i < pawns.length; i++) {
-            if (_pawn.id == pawns[i].id) {
-                this.pIndex = i;
-                break;
-            }
+    SetActive(piece) {
+        var pieces = this.pieces;
 
+        outer: for (var i = 0; i < pieces.length; i++) {
+            for (var u = 0; u < pieces[i].length; u++) {
+                if (pieces[i][u].id === piece.id) {
+                    this.pIndex1 = i;
+                    this.pIndex2 = u;
+                    break outer; 
+                }
+            }
         }
+
+        //for (i = 0; i < pawns.length; i++) {
+        //    if (_pawn.id == pawns[i].id) {
+        //        this.pIndex = i;
+        //        break;
+        //    }
+
+        //}
     }
 
-    MovePiece(_pawn) {
+    MovePiece(_piece) {
         //moving pawn to coordinates that other player has moved it to
         //  var i = this.pIndex;
         //var x = this.player;
-        var pawn = this.opponent[this.pIndex];
-        pawn.setTranslate(_pawn.left, _pawn.top);
+        var piece = this.pieces[this.pIndex1][this.pIndex2];
+        piece.setTranslate(_piece.left, _piece.top);
         //updating pawn info to that of the other player
-        pawn.xIndexCur = _pawn.xIndexCur;
-        pawn.yIndexCur = _pawn.yIndexCur;
-        pawn.left = _pawn.left;
-        pawn.top = _pawn.top;
+        piece.xIndexCur = _piece.xIndexCur;
+        piece.yIndexCur = _piece.yIndexCur;
+        piece.left = _piece.left;
+        piece.top = _piece.top;
 
 
 
